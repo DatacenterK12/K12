46838c26607f1f92d5ae9871d8e7c64f6e9c463e
Bulking <sergey43554@gmail.com>
2023-02-27 17:52:32 +0300

    Optimized form call js

--- K12/static/js/form.js
+++ K12/static/js/form.js
@@ -1,12 +1,11 @@
 "use strict"
 
 document.addEventListener('DOMContentLoaded', function () {
-    const close = document.getElementById('button-close');
     const form = document.getElementById('call-form');
-    
+    const callthanks = document.querySelector("#call-thanks");
     form.addEventListener('submit', function(){
         const call = document.querySelector("#call1");
-        call.outerHTML = '<div class="modal-header"><button type="button" class="btn-close" data-bs-dismiss="modal">×</button><h4 class="modal-title">Спасибо за заявку!</h4></div><div class="modal-body" style="background-size: cover"><div class="form-group" style="background-size: cover">Пожалуйста, ожидайте звонка нашего специалиста!</div></div></div>';
+        call.outerHTML = callthanks.outerHTML;
     })
     form.addEventListener('submit1', formSend);
 
